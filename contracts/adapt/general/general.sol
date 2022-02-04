55f9eac13438ae5a689d5745d959523130289763
Kieran Mesquita <mesquitakieran@gmail.com>
2022-02-04 19:05:17 +1100

    Generalizecall steps

--- contracts/adapt/general/general.sol
+++ contracts/adapt/general/general.sol
@@ -31,6 +31,19 @@ contract GeneralAdapt {
   RailgunLogic public railgun;
 
   /**
+   * @notice Blocks calls from external contracts
+   */
+  modifier noExternalContract () {
+    // This prevents malicious contracts that are being interacted with as part of a multicall
+    // from being able to steal funds via send or deposit via callbacks
+    require(
+      msg.sender == tx.origin
+      || msg.sender == address(this)
+      , "GeneralAdapt: Caller is external contract"
+    )
+  }
+
+  /**
    * @notice Sets Railgun contract address
    */
   constructor(RailgunLogic _railgun) {
@@ -38,54 +51,14 @@ contract GeneralAdapt {
   }
 
   /**
-   * @notice Executes a batch of Railgun transactions followed by a multicall and finally a deposit back to Railgun
-   * @param _transactions - Batch of Railgun transactions to execute
-   * @param _random - Random value (should be less than snark scalar field and shouldn't be reused if resubmitting the same transaction through another relayer)
-   * @param _requireSuccess - Whether transaction should throw on failure
+   * @notice Executes multicall batch
+   * @param _requireSuccess - Whether transaction should throw on multicall failure
    * @param _calls - multicall
-   * @param _deposits - array of tokens to deposit back to Railgun once transaction is complete
-   * @param _pubkey - pubkey of Railgun account to deposit back to
    */
-  function execute(
-    Transaction[] calldata _transactions,
-    // In an edge case where railgun transactions are submitted into the mempool
-    // but not mined followed by a different set of railgun transactions submitted
-    // to the mempool with the same calls, an advisary could mix and match
-    // transactions as long as the total transaction count remains the same
-    // Including the random factor here prevents this from happening
-    uint256 _random,
+  function multicall(
     bool _requireSuccess,
     Call[] calldata _calls,
-    IERC20[] calldata _deposits,
-    uint256[2] calldata _pubkey
-  ) public returns (Result[] memory) {
-    // Calculate the expected adaptID parameters value
-    // The number of transactions is included here to ensure railgun transactions can't be removed
-    // by an advisary while the transaction is still in the mempool
-    uint256 adaptIDparameters = uint256(
-      sha256(
-        abi.encode(
-          _transactions.length,
-          _random,
-          _requireSuccess,
-          _calls,
-          _deposits,
-          _pubkey
-        )
-      )
-    );
-
-    // Loop through each transaction and en
-    for(uint256 i = 0; i < _transactions.length; i++) {
-      require(_transactions[i].adaptIDparameters == adaptIDparameters, "GeneralAdapt: AdaptID Parameters Mismatch");
-    }
-
-    // Check adapt parameters
-    _transactions[0].adaptIDparameters;
-
-    // Execute initial railgun transaction
-    railgun.transact(_transactions);
-
+  ) public noExternalContract returns (Result[] memory) {
     // Initialize returnData array
     Result[] memory returnData = new Result[](_calls.length);
 
@@ -111,9 +84,51 @@ contract GeneralAdapt {
       returnData[i] = Result(success, ret);
     }
 
-    // Initialize generated deposits array
-    GeneratedCommitment[] memory generatedDeposits = new GeneratedCommitment[](_deposits.length);
+    return returnData;
+  }
 
+   /**
+   * @notice Executes a batch of Railgun transactions
+   * @param _transactions - Batch of Railgun transactions to execute
+   * @param _additionalData - additional data for transaction adaptID, if calling this function directly this
+   * should be a random value (shouldn't be reused if resubmitting the same transaction through another relayer)
+   */
+  function railgunBatch(
+    Transaction[] calldata _transactions,
+    bytes memory _additionalData
+  ) public noExternalContract {
+    // Calculate the expected adaptID parameters value
+    // The number of transactions is included here to ensure railgun transactions can't be removed
+    // by an advisary while the transaction is still in the mempool
+    uint256 adaptIDparameters = uint256(
+      sha256(
+        abi.encode(
+          _transactions.length,
+          _additionalData
+        )
+      )
+    );
+
+    // Loop through each transaction and ensure adaptID parameters match
+    for(uint256 i = 0; i < _transactions.length; i++) {
+      require(_transactions[i].adaptIDparameters == adaptIDparameters, "GeneralAdapt: AdaptID Parameters Mismatch");
+    }
+
+    // Execute railgun transactions
+    railgun.transact(_transactions);
+  }
+
+  /**
+   * @notice Executes a batch of Railgun deposits
+   * @param _deposits - ERC20 tokens to deposit
+   * @param _random - Random value (should be less than snark scalar field)
+   * @param _pubkey - public key to deposit tokens to
+   */
+  function deposit(
+    IERC20[] calldata _deposits,
+    uint256 _random,
+    uint256[2] calldata _pubkey
+  ) public noExternalContract {
     // Loop through each token specified for deposit and deposit our total balance
     // Due to a quirk with the USDT token contract this will fail if it's approval is
     // non-0 (https://github.com/Uniswap/interface/issues/1034), to ensure that your
@@ -142,6 +157,84 @@ contract GeneralAdapt {
 
     // Deposit back to Railgun
     railgun.generateDeposit(generatedDeposits);
+  }
+
+  /**
+   * @notice Sends tokens to particular address
+   * @param _tokens - ERC20 tokens to send
+   * @param _to - ETH address to send to
+   */
+   function send(
+    IERC20[] calldata _tokens,
+    address _to
+  ) public noExternalContract {
+    // Loop through each token specified for deposit and deposit our total balance
+    // Due to a quirk with the USDT token contract this will fail if it's approval is
+    // non-0 (https://github.com/Uniswap/interface/issues/1034), to ensure that your
+    // transaction always succeeds when dealing with USDT/similar tokens make sure the last
+    // call in your calls is a call to the token contract with an approval of 0
+    for (uint256 i = 0; i < _calls.length; i++) {
+      IERC20 token = _deposits[i];
+
+      // Fetch balance
+      uint256 balance = token.balanceOf(address(this));
+
+      // Approve the balance for deposit
+      token.safeApprove(
+        address(railgun),
+        balance
+      );
+
+      // Push to deposits array
+      generatedDeposits[i] = GeneratedCommitment({
+        pubkey: _pubkey,
+        random: _random,
+        amount: balance,
+        token: address(token)
+      });
+    }
+
+    // Deposit back to Railgun
+    railgun.generateDeposit(generatedDeposits);
+  }
+
+  /**
+   * @notice Executes a batch of Railgun transactions followed by a multicall
+   * @param _transactions - Batch of Railgun transactions to execute
+   * @param _random - Random value (shouldn't be reused if resubmitting the same transaction through another relayer)
+   * @param _requireSuccess - Whether transaction should throw on multicall failure
+   * @param _calls - multicall
+   */
+  function relay(
+    Transaction[] calldata _transactions,
+    // In an edge case where railgun transactions are submitted into the mempool
+    // but not mined followed by a different set of railgun transactions submitted
+    // to the mempool with the same calls, an advisary could mix and match
+    // transactions as long as the total transaction count remains the same
+    // Including the random factor here prevents this from happening
+    uint256 _random,
+    bool _requireSuccess,
+    Call[] calldata _calls,
+  ) public returns (Result[] memory) {
+    // Calculate additionalData parameter for adaptID parameters
+    uint256 additionalData = uint256(
+      sha256(
+        abi.encode(
+          _random,
+          _requireSuccess,
+          _calls
+        )
+      )
+    );
+
+    // Executes railgun batch
+    railgunBatch(_transactions, additionalData);
+
+    // Execute multicalls
+    Result[] memory returnData = multicall(requireSuccess, _calls);
+
+    // To execute a multicall and deposit or send the result, encode a call to the relevant function on this
+    // contract at the end of your calls array.
 
     // Return returnData
     return returnData;
