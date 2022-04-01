7612c738935200843790dac8a76a82af3de550ea
Kieran Mesquita <mesquitakieran@gmail.com>
2022-04-01 15:08:42 +0800

    WIP verifier transaction test

--- test/logic/verififer.js
+++ test/logic/verififer.js
@@ -1,4 +1,4 @@
-/* global describe it beforeEach ethers */
+/* global hre describe it beforeEach ethers */
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
@@ -7,13 +7,23 @@ chai.use(chaiAsPromised);
 const { expect } = chai;
 
 const artifacts = require('../../helpers/snarkKeys');
+const transaction = require('../../helpers/transaction');
 
 let verifier;
+let snarkBypassSigner;
+let verifierBypassSigner;
 
 describe('Logic/Verifier', () => {
   beforeEach(async () => {
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: ['0x0000000000000000000000000000000000000000'],
+    });
+    snarkBypassSigner = await ethers.getSigner('0x0000000000000000000000000000000000000000');
+
     const VerifierStub = await ethers.getContractFactory('VerifierStub');
     verifier = await VerifierStub.deploy();
+    verifierBypassSigner = verifier.connect(snarkBypassSigner);
   });
 
   it('Should set verifying key', async () => {
@@ -41,5 +51,9 @@ describe('Logic/Verifier', () => {
     expect(key.delta2.x[0]).to.equal(solidityVkey.delta2.x[0]);
     expect(key.gamma2.x[0]).to.equal(solidityVkey.gamma2.x[0]);
     expect(key.ic[0].x).to.equal(solidityVkey.ic[0].x);
+  });
+
+  it('Should verify proof', async () => {
+    const tx = transaction.transact();
   });
 });
