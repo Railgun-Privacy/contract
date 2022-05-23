342ecc3e021fc9bf81df2323a648f58acd9f5367
Kieran Mesquita <mesquitakieran@gmail.com>
2022-05-23 15:19:55 +0800

    RelayAdapt getAdaptParams

--- helpers/adapt/relay/relayadapt.js
+++ helpers/adapt/relay/relayadapt.js
@@ -5,19 +5,17 @@ const abiCoder = ethers.utils.defaultAbiCoder;
 /**
  * Get adapt params field
  *
- * @param {object[]} transactions - bound parameters struct
+ * @param {object[]} transactions - transactions
  * @param {string} additionalData - additional byte data to add to adapt params
  * @returns {string} adapt params
  */
 function getAdaptParams(transactions, additionalData) {
-  console.log(transactions);
   const firstNullifiers = transactions.map((transaction) => transaction.nullifiers[0]);
 
-  console.log(firstNullifiers);
-
-  // return ethers.utils.keccak256(abiCoder.encode([
-  //   'tuple(uint16 treeNumber, uint8 withdraw, address adaptContract, bytes32 adaptParams, tuple(uint256[4] ciphertext, uint256[2] ephemeralKeys, uint256[] memo)[] commitmentCiphertext) _boundParams',
-  // ], [boundParams]));
+  return ethers.utils.keccak256(abiCoder.encode(
+    ['uint256[]', 'uint256', 'bytes'],
+    [firstNullifiers, transactions.length, additionalData],
+  ));
 }
 
 function getRelayAdaptParams() {
