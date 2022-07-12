3a01a70d40360a59c897574b5a6d8a8ee9a33b1e
Kieran Mesquita <mesquitakieran@gmail.com>
2022-07-12 11:38:11 +1000

    Bugfix initialization

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -32,7 +32,7 @@ contract Treasury is Initializable, AccessControlUpgradeable {
     AccessControlUpgradeable._grantRole(DEFAULT_ADMIN_ROLE, _admin);
 
     // Give owner the transfer role
-    AccessControlUpgradeable.grantRole(TRANSFER_ROLE, _admin);
+    AccessControlUpgradeable._grantRole(TRANSFER_ROLE, _admin);
   }
 
   /**
