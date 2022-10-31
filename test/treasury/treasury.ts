2139c3cca24c561f4c0a32aa2ba711c38cb9290f
Kieran Mesquita <me@mesquka.com>
2022-11-01 02:13:58 +0800

    Expand treasury test coverage

--- test/treasury/treasury.ts
+++ test/treasury/treasury.ts
@@ -12,7 +12,11 @@ describe('Treasury/Treasury', () => {
     const Treasury = await ethers.getContractFactory('Treasury');
     const ERC20 = await ethers.getContractFactory('TestERC20');
 
+    // Get signers
+    const [primaryAccount, secondaryAccount] = await ethers.getSigners();
+
     const treasury = await Treasury.deploy();
+    const treasurySecondary = treasury.connect(secondaryAccount);
 
     // Initialize treasury with signer 0 as admin
     await treasury.initializeTreasury((await ethers.getSigners())[0].address);
@@ -30,15 +34,22 @@ describe('Treasury/Treasury', () => {
     // Give ETH to treasury
     await setBalance(treasury.address, 10000);
 
-    return { treasury, erc20 };
+    return { treasury, treasurySecondary, erc20, primaryAccount, secondaryAccount };
   }
 
   it('Should transfer ETH', async () => {
-    const { treasury } = await loadFixture(deploy);
+    const { treasury, treasurySecondary, secondaryAccount } = await loadFixture(deploy);
 
     // Should prevent burning
     await expect(treasury.transferETH(ethers.constants.AddressZero, 1000)).to.be.revertedWith(
       'Treasury: Preventing accidental burn',
+    );
+
+    // Should prevent accounts without role from calling
+    await expect(
+      treasurySecondary.transferETH((await ethers.getSigners())[0].address, 1000),
+    ).to.be.revertedWith(
+      `AccessControl: account ${secondaryAccount.address.toLowerCase()} is missing role ${await treasury.TRANSFER_ROLE()}`,
     );
 
     // Transfer should adjust balances
@@ -59,12 +70,19 @@ describe('Treasury/Treasury', () => {
   });
 
   it('Should transfer ERC20', async () => {
-    const { treasury, erc20 } = await loadFixture(deploy);
+    const { treasury, treasurySecondary, erc20, secondaryAccount } = await loadFixture(deploy);
 
     // Should prevent burning
     await expect(
       treasury.transferERC20(erc20.address, ethers.constants.AddressZero, 1000),
     ).to.be.revertedWith('Treasury: Preventing accidental burn');
+
+    // Should prevent accounts without role from calling
+    await expect(
+      treasurySecondary.transferERC20(erc20.address, (await ethers.getSigners())[0].address, 1000),
+    ).to.be.revertedWith(
+      `AccessControl: account ${secondaryAccount.address.toLowerCase()} is missing role ${await treasury.TRANSFER_ROLE()}`,
+    );
 
     // Transfer should adjust balances
     await expect(
@@ -74,5 +92,13 @@ describe('Treasury/Treasury', () => {
       [treasury.address, (await ethers.getSigners())[0].address],
       [-1000, 1000],
     );
+  });
+
+  it("Shouldn't double init", async () => {
+    const { treasury } = await loadFixture(deploy);
+
+    await expect(
+      treasury.initializeTreasury((await ethers.getSigners())[0].address),
+    ).to.be.revertedWith('Initializable: contract is already initialized');
   });
 });
