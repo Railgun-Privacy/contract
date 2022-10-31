0417fca331c514f632d21a5196f26cd6865ae625
Kieran Mesquita <me@mesquka.com>
2022-11-01 02:14:37 +0800

    Expand proxyadmin test coverage

--- test/proxy/proxyadmin.ts
+++ test/proxy/proxyadmin.ts
@@ -13,8 +13,12 @@ describe('Proxy/ProxyAdmin', () => {
     const ProxyAdmin = await ethers.getContractFactory('ProxyAdmin');
     const TargetA = await ethers.getContractFactory('ProxyTargetStubA');
 
+    // Get signers
+    const [primaryAccount, secondaryAccount] = await ethers.getSigners();
+
     // Get admin interface
     const proxyAdmin = await ProxyAdmin.deploy((await ethers.getSigners())[0].address);
+    const proxyAdminSecondary = proxyAdmin.connect(secondaryAccount);
 
     // Deploy proxy
     const proxy = await Proxy.deploy(proxyAdmin.address);
@@ -28,13 +32,27 @@ describe('Proxy/ProxyAdmin', () => {
     return {
       proxy,
       proxyAdmin,
+      proxyAdminSecondary,
       target,
       targetA,
+      primaryAccount,
+      secondaryAccount,
     };
   }
 
   it('Should upgrade and unpause', async () => {
-    const { proxy, proxyAdmin, target, targetA } = await loadFixture(deploy);
+    const { proxy, proxyAdmin, proxyAdminSecondary, target, targetA } = await loadFixture(deploy);
+
+    // Non-owner shouldn't be able to call pause/unpause/upgrade
+    await expect(proxyAdminSecondary.pause(proxy.address)).to.be.revertedWith(
+      'Ownable: caller is not the owner',
+    );
+    await expect(proxyAdminSecondary.unpause(proxy.address)).to.be.revertedWith(
+      'Ownable: caller is not the owner',
+    );
+    await expect(proxyAdminSecondary.upgrade(proxy.address, targetA.address)).to.be.revertedWith(
+      'Ownable: caller is not the owner',
+    );
 
     // Deployment should begin paused
     await expect(target.identify()).to.be.revertedWith('Proxy: Contract is paused');
@@ -52,7 +70,17 @@ describe('Proxy/ProxyAdmin', () => {
   });
 
   it('Should transfer proxy ownership', async () => {
-    const { proxy, proxyAdmin } = await loadFixture(deploy);
+    const { proxy, proxyAdmin, proxyAdminSecondary } = await loadFixture(deploy);
+
+    // Non-owner shouldn't be able to call transfer ownership
+    await expect(
+      proxyAdminSecondary.transferProxyOwnership(
+        proxy.address,
+        (
+          await ethers.getSigners()
+        )[0].address,
+      ),
+    ).to.be.revertedWith('Ownable: caller is not the owner');
 
     // Transfer ownership
     await expect(
