2f2ce738ca97d9efa1f1ab02cfe4120df0f7b501
Kieran <me@mesquka.com>
2025-07-14 14:50:14 +0000

    Verify script fix

--- tasks/verify.ts
+++ tasks/verify.ts
@@ -52,7 +52,7 @@ task('verify:deployment', 'Verifies deployment parameters on chain')
     expect(
       (
         await railgun.provider.getStorageAt(
-          railgun.address,
+          addresses.proxy,
           '0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103', // bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)
         )
       ).slice(26, 66),
@@ -64,11 +64,11 @@ task('verify:deployment', 'Verifies deployment parameters on chain')
     expect(
       (
         await railgun.provider.getStorageAt(
-          railgun.address,
+          addresses.treasuryProxy,
           '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc', // bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1))
         )
       ).slice(26, 66),
-    ).to.equal(addresses.implementation.slice(2, 44).toLowerCase());
+    ).to.equal(addresses.treasuryImplementation.slice(2, 44).toLowerCase());
 
     console.log(
       `Ensuring railgun smart wallet (${addresses.proxy}) is owned by delegator (${addresses.delegator})`,
@@ -83,7 +83,7 @@ task('verify:deployment', 'Verifies deployment parameters on chain')
     expect(
       (
         await treasury.provider.getStorageAt(
-          treasury.address,
+          addresses.treasuryProxy,
           '0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103', // bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)
         )
       ).slice(26, 66),
@@ -95,7 +95,7 @@ task('verify:deployment', 'Verifies deployment parameters on chain')
     expect(
       (
         await treasury.provider.getStorageAt(
-          treasury.address,
+          addresses.treasuryProxy,
           '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc', // bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1))
         )
       ).slice(26, 66),
